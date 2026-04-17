eb492725e99a22d16ec4c46347d6c7594d920d1b
pipitlusianawati <lusianawatipipit@gmail.com>
2026-04-18 00:02:02 +0700

    Implement typing effect for introduction text
    Added typing effect for two lines of text on page load.

--- script.js
+++ script.js
@@ -5,3 +5,31 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
             .scrollIntoView({ behavior: 'smooth' });
     });
 });
+
+const line1 = "I'm Pipit Lusianawati";
+const line2 = "Water Engineer | Hydraulic Analyst | Hydraulic Analysis Enthusiast";
+
+let i = 0;
+let j = 0;
+
+function typeLine1() {
+    if (i < line1.length) {
+        document.getElementById("typing-line1").innerHTML += line1.charAt(i);
+        i++;
+        setTimeout(typeLine1, 60);
+    } else {
+        setTimeout(typeLine2, 300);
+    }
+}
+
+function typeLine2() {
+    if (j < line2.length) {
+        document.getElementById("typing-line2").innerHTML += line2.charAt(j);
+        j++;
+        setTimeout(typeLine2, 40);
+    }
+}
+
+window.onload = function() {
+    typeLine1();
+};
